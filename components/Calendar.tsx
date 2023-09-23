02368deeb0252a75b35fe8129c7fa56ab703ac9d
racalixta <ra.calixta@gmail.com>
2023-09-23 16:14:02 -0300

    feat: previous and next button

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import ArrowIcon from './ArrowIcon'
 
 function getDaysInMonth(month: number, year: number) {
@@ -25,19 +27,53 @@ const currentYear =  currentDate.getFullYear();
 const weekDays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
 
 const Calendar = () => {
-  const daysInMonth = getDaysInMonth(currentMonth, currentYear); 
-  
+  const [month, setMonth] = useState(currentMonth);
+  const [year, setYear] = useState(currentYear);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [daysInMonth, setDaysInMonth] = useState(getDaysInMonth(currentMonth, currentYear));
+
+  useEffect(() => {
+    setDaysInMonth(getDaysInMonth(month, year));
+    setSelectedDate(new Date(year, month, 1));
+  }, [month, year])
+
+  function goToPreviousMonth() {
+    if(month === 0) {
+      setYear(year - 1);
+      setMonth(11); 
+    } else {
+      setMonth(month - 1);
+    }
+  }
+
+  function goToNextMonth() {
+    if(month === 11) {
+      setYear(year + 1);
+      setMonth(0); 
+    } else {
+      setMonth(month + 1);
+    }
+  }
+
+  function getFullDateString() {
+    const monthName = selectedDate.toLocaleString("pt-Br", {month: "long"});
+    const upperCaseMonthName = monthName[0].toUpperCase() + monthName.slice(1);
+    return `${upperCaseMonthName} de ${selectedDate.getFullYear()}`;
+
+  }
+
+
   return(
     <section className="2-full rounded-md bg-neutral-800">
 
         <div className="flex justify-between mx-2 my-4 font-sans text-neutral-300">
-          <button>
+          <button onClick={() => goToPreviousMonth()}>
             <ArrowIcon className="stroke-neutral-300" />
           </button>
 
-          <span>Setembro de 2023</span>
+          <span>{getFullDateString()}</span>
 
-          <button >
+          <button onClick={() => goToNextMonth()}>
             <ArrowIcon className="rotate-180 stroke-neutral-300" />
           </button>
         </div>
